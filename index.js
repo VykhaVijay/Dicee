66dc7376c13ca28fcb456af703fdcad46a57c7f8
VykhaVijay <83864456+VykhaVijay@users.noreply.github.com>
2022-05-25 00:13:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 var randomNumber1 = Math.floor(Math.random()*6 + 1); //+1 because 1 is exclusive [0,1)
-// var image = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png",
-//"images/dice6.png"]; maybe one way
 
 document.querySelector(".img1").setAttribute("src", "dice"+ randomNumber1 +".png");
 
 var randomNumber2 = Math.floor(Math.random()*6 + 1);
 
-//change image
 document.querySelector(".img2").setAttribute("src", "dice"+ randomNumber2+".png");
 
-//change text
 if(randomNumber1===6)
-{
-    //document.getElementsByTagName("h1")[0].innerText = "Player 1 Wins"; correct but want to use queryselector 
+{ 
     document.querySelector("h1").innerText = "🚩Player 1 Wins!";
 }
 if(randomNumber2===6)
